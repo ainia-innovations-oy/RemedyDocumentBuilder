1e501783938a4dbba53735dacf8caee74a072212
Daryoush Farsimadan <daryoush.farsimadan@ainia.fi>
2023-06-29 09:04:12 +0300

    Add licenses into source files.

--- DocumentBuilderNet/Program.cs
+++ DocumentBuilderNet/Program.cs
@@ -1,4 +1,24 @@
-﻿using DocumentBuilderNet;
+﻿
+/*
+    Creates a console application for PDF Generation that accepts 2 parameters: <Template Path, Output Path>  
+
+    Copyright (C) 2023 Ainia Solutions Oy
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using DocumentBuilderNet;
 
 string path = Environment.GetCommandLineArgs()[1];
 string outFile = Environment.GetCommandLineArgs()[2];
--- DocumentBuilderNet/RecData.cs
+++ DocumentBuilderNet/RecData.cs
@@ -1,10 +1,26 @@
-﻿using iText.Kernel.Colors;
-using iText.Kernel.Pdf.Canvas;
+﻿
+/*
+    Implements two support functions.  
+
+
+    Copyright (C) 2023  Ainia Solutions Oy
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
 using iText.Kernel.Pdf;
-using iText.Layout.Element;
-using iText.Layout.Properties;
-using System;
-using iText.Layout;
 using iText.Html2pdf;
 using iText.StyledXmlParser.Css.Media;
 
@@ -13,7 +29,6 @@ namespace DocumentBuilderNet
 
     internal class RecData
     {
-
         public static string GetTemplate(string path)
         {
             var stream = new FileStream(path, FileMode.Open);
@@ -26,11 +41,6 @@ namespace DocumentBuilderNet
 
            
             return tmp;
-        }
-
-        public static void DeleteHtmlFile(string path)
-        {
-            File.Delete(path);
         }
 
         public static void CreatePDFFIle(string template, string dest)
